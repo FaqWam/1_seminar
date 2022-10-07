4e6a7cabde4fad7a9e781b7107fb9a4d12fe0b78
FaqWam <114764212+FaqWam@users.noreply.github.com>
2022-10-07 14:50:00 +0300

    2 задача

---
+++ Program3.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+string value = Console.ReadLine();
+
+Console.WriteLine("Введите второе число");
+string value2 = Console.ReadLine();
+
+int a = int.Parse(value);
+int b = int.Parse(value2);
+
+if  (a>b){
+            Console.Write(value);
+}else{Console.Write(value2);
+}
