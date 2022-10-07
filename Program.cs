7b19b36b67643668e57e68aa13fd44d5bbfaddda
FaqWam <114764212+FaqWam@users.noreply.github.com>
2022-10-07 14:39:01 +0300

    2 задача

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число");
+string value = Console.ReadLine();
+
+Console.WriteLine("Введите второе число");
+string value2 = Console.ReadLine();
+
+Console.WriteLine("Введите третье число");
+string value3 = Console.ReadLine();
+
+int a = int.Parse(value);
+int b = int.Parse(value2);
+int c = int.Parse(value3);
+
+if (a>b && a>c)
+{Console.Write(value);}
+if (a>b && a<c)
+{Console.Write(value3);}
+if (a<b && b>c)
+{Console.Write(value2);}
+if (a<b && b<c)
+{Console.Write(value3);}
